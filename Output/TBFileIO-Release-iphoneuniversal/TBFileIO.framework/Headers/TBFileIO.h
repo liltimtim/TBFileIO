66390a693d82123602f0e821761d50bd5342f98e
Timothy Barrett <tbarrett@taxslayer.com>
2016-04-06 14:13:57 -0400

    pushing up pod stuff

---
+++ Output/TBFileIO-Release-iphoneuniversal/TBFileIO.framework/Headers/TBFileIO.h
@@ -0,0 +1,19 @@
+//
+//  TBFileIO.h
+//  TBFileIO
+//
+//  Created by Timothy Barrett on 4/6/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TBFileIO.
+FOUNDATION_EXPORT double TBFileIOVersionNumber;
+
+//! Project version string for TBFileIO.
+FOUNDATION_EXPORT const unsigned char TBFileIOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TBFileIO/PublicHeader.h>
+
+
